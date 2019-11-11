9e0cfc948406e35a1788e5216e76e5f5df2c6881
Josearratia <1630037@upv.edu.mx>
2019-11-10 21:47:47 -0600

    Modificacion de nombre de fx scene a inicio de sesion

--- Proyecto_Final/src/main/java/com/proyecto_final/MainApp.java
+++ Proyecto_Final/src/main/java/com/proyecto_final/MainApp.java
@@ -12,12 +12,12 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Inicio_De_Sesion.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Inicio De Sesion");
         stage.setScene(scene);
         stage.show();
     }
